0a150953485ba135fdc940d785833ae2b508b21b
unknown <greenmmanalishi2022@gmail.com>
2022-06-23 18:48:35 -0400

    Cambie de plotly a leaflet para el cloro, esta mucho mejor asi, MEJORAR EL TEMA Y LOS COLORES, PROVEER INFO SOBRE EL MAPA Y AJUSTAR LA ESCALA DE COLORES

--- app.R
+++ app.R
@@ -3,6 +3,8 @@ library(shinydashboard)
 library(plotly)
 library(shinyWidgets)
 library(tidyr)
+library(dplyr)
+library(leaflet)
 source('testing.R')
 source("get_entities.R")
 source("get_outages.R")
@@ -61,7 +63,7 @@ ui <- dashboardPage(
   dashboardBody(
     fluidRow(
       box(plotlyOutput("timeseries"), width = 12, height = 580),
-      box(plotlyOutput("mapa"), width = 12, height = 580),
+      box(leafletOutput("mapa"), width = 12, height = 580),
       box(textOutput("text"))
     )
   )
@@ -113,26 +115,51 @@ output$text<- renderPrint(input$Id009)
     p
   })
   
-  output$mapa <- renderPlotly({
+  bins <- c(0, 10, 20, 50, 100, 200, 500, 1000, Inf)
+  pal <- colorBin("YlOrRd", domain = venequia$score, bins = bins)
 
-
-    geojson <- rjson::fromJSON(file = "geojson/venezuela.geojson")
-    g<- list(
-      #scope = "south america",
-      fitbounds = "locations"
-    )
-    fig <- plot_ly()
-    fig <- fig %>% add_trace(
-      type="choropleth",
-      geojson=geojson,
-      locations=outages$location_name,
-      z = outages$score,
-      featureidkey = "properties.ESTADO",
-      colors = "Reds"
-    ) %>% 
-      layout(geo = g)
-    fig
+  labels <- venequia$LABEL %>% lapply(htmltools::HTML)
+  output$mapa <- renderLeaflet({
+    
+    leaflet(venequia) %>% 
+      addTiles() %>% 
+      addPolygons(
+        fillColor= ~pal(score),
+        weight = 2,
+        opacity = 1,
+        color = "black",
+        dashArray = "3",
+        fillOpacity = 0.7,
+        label = labels
+      )%>% addLegend(pal = pal, values = ~score, opacity = 0.7, title = NULL,
+                     position = "bottomright")
+    
+     
   })
+  
+  # 
+  # output$mapa <- renderPlotly({
+  # 
+  # 
+  #   geojson <- rjson::fromJSON(file = "geojson/venezuela.geojson")
+  #   g<- list(
+  #     #scope = "south america",
+  #     fitbounds = "locations"
+  #   )
+  #   fig <- plot_ly()
+  #   fig <- fig %>% add_trace(
+  #     type="choropleth",
+  #     geojson=geojson,
+  #     locations=outages$location_name,
+  #     z = outages$score,
+  #     featureidkey = "properties.ESTADO",
+  #     colors = "Reds"
+  #   ) %>% 
+  #     layout(geo = g,
+  #            title = " <b> Caidas de internet por region </b>",
+  #            legend = list(xanchor = "center", x = 0.5, y = -0.15, orientation = 'h'))
+  #   fig
+  # })
 
   
   
--- get_outages.R
+++ get_outages.R
@@ -1,3 +1,4 @@
+venequia <- geojsonio::geojson_read("geojson/venezuela.geojson",what = 'sp')
 
 until <- unix_from_date(now() + hours(4))
 from <- unix_from_date(now() - days(1) + hours(4)) #VERIFICAR ESO EL DESPLAZAMIENTO, LA FUNCION UNIX_... PARECE TENER UN OFFSET
@@ -16,12 +17,15 @@ fetch_data(url) -> outages
 
 outages <- outages %>% 
   separate(location,c("type","code"),sep = "/") %>% 
-  filter(type == "region") %>% 
-  right_join(entities, by = c("location_name" = "name"))
+  filter(type == "region") %>%
+  rename( ESTADO = location_name) %>% 
+  mutate(LABEL = paste("<strong>",ESTADO,"</strong>","-",as.integer(score))) %>% 
+  select(ESTADO,score,LABEL)
+  # right_join(entities, by = c("location_name" = "name"))
+  # 
 
+#outages$score[is.na(outages$score)] <- 0
 
-outages$score[is.na(outages$score)] <- 0
+outages$ESTADO <-  toupper(iconv(outages$ESTADO,to = 'ASCII//TRANSLIT'))
 
-outages$location_name <-  toupper(iconv(outages$location_name,to = 'ASCII//TRANSLIT'))
-
-
+venequia <- sp::merge(venequia, outages, by ="ESTADO",all.x = FALSE)
